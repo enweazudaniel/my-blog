746921c4aed997cfd63e4cad491f0d74c29c5ca2
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-02 22:22:43 +0100

    for loop

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/" function(req, res){
-  res.render("home", {startingContent: homeStartingContent });
+  res.render("home", {startingContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function(req, res){
@@ -35,7 +37,9 @@ app.post("/compose", function(req, res){
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
-  }
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 
