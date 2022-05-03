95f0729af0836f560ba12633167caee3bce045c7
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-03 15:47:52 +0100

    install node

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/" function(req, res){
+app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent, posts: posts });
 });
 
