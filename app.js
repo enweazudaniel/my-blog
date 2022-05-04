e0def081f7e791091d2c330b0d6216e198d96b41
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-05 00:27:32 +0100

    Add PORT

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.get("/posts/:postName", function(req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
